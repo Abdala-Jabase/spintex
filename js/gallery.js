b4844c84f1669cd4a66cfc431ba607d926fd930a
Abdala Jabase <abdala.jabase@gmail.com>
2022-07-15 18:56:02 +0200

    Fix close button appearing before picture opening

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,9 @@
 function myFunction(imgs) {
+    var closeBtn = document.getElementById("closebtn");
     var expandImg = document.getElementById("expandedImg");
     expandImg.src = imgs.src;
     expandImg.parentElement.style.display = "block";
+    closeBtn.style.display = "inline";
     document.querySelector('#expandedImg').scrollIntoView({
         behavior: 'smooth'
       });
